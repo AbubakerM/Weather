884520a8562117924047eee36165da225c0b23eb
abubaker <eng.mhabubaker@gmail.com>
2022-03-05 14:25:07 +0400

    - Project Start Folders added

---
+++ Weather/Controller/DetailsVC/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  Weather
+//
+//  Created by 118 on 2022/3/5.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weather/Supporting/Constants/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by 118 on 2022/3/5.
+//
+
+import Foundation
+
+struct Constants {}
